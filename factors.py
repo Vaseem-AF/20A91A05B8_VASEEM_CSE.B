50d5f169c7bcac6b0019b5be505c76dddce4b64d
Vaseem-AF <84456545+Vaseem-AF@users.noreply.github.com>
2021-05-29 13:32:47 +0530

    Add files via upload

---
+++ factors.py
@@ -0,0 +1,14 @@
+#Factors of a number
+
+n=int(input('Enter the number whose factors is to be found '))
+i=1
+print('The factors of numbers are : ')
+while i in range (i,n+1):
+       if (n%i==0):
+        print(i)
+        i=i+1
+
+       else :
+        i=i+1
+        
+         
