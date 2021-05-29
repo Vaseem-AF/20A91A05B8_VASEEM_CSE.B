d0da33def1c3d6f98f15be634c5809a4abf6792a
Vaseem-AF <84456545+Vaseem-AF@users.noreply.github.com>
2021-05-29 13:44:05 +0530

    Update prime.or.not.py

--- prime.or.not.py
+++ prime.or.not.py
@@ -16,3 +16,20 @@ if (count==2):
     print('a prime number')
 else :
     print('not a prime number')
+    
+    
+    
+#output :
+Enter the number 5
+The factors of  5 are : 
+1 5 
+since the number has 2 factors it is 
+a prime number
+
+
+Enter the number 6
+The factors of  6 are : 
+1 2 3 6 
+since the number has 4 factors it is 
+not a prime number
+    
