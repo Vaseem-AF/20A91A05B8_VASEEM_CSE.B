5dbfa7a2719e05e777a6eb2af0a758dbfeedd19c
Vaseem-AF <84456545+Vaseem-AF@users.noreply.github.com>
2021-05-29 13:40:10 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,10 +1,14 @@
 #Reverse of the number
 
-number=int(input('enter the number to be reversed '))
+number=int(input('Enter the number '))
 summation=0
 while (number!=0):
     remainder=number%10
     summation=summation*10+remainder
     number=number//10
 
-print('The reversed number is %d' %summation)    
+print('The reverse number is %d' %summation)    
+
+#output :
+Enter the number 67347
+The reverse number is 74376
